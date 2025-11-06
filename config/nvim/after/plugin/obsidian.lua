c1812b207151fabce995a91a578606d368c573cf
Christian Bingman <christia@meraki.com>
2025-11-06 11:46:52 -0800

    Add more workspaces

--- config/nvim/after/plugin/obsidian.lua
+++ config/nvim/after/plugin/obsidian.lua
@@ -4,6 +4,14 @@ require("obsidian").setup({
       name = "Meraki",
       path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Meraki"
     },
+    {
+      name = "Personal",
+      path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Personal"
+    },
+    {
+      name = "Personal Linux",
+      path = "~/Documents/Obsidian/Personal"
+    },
   },
   legacy_commands = false,
 })
